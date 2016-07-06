0e5ac16f89f5fbd611f71515a91bad37febd4083
rebeccabartels <rebeccakbartels@icloud.com>
2016-07-06 14:41:59 -0400

    uppercase string method

--- Help.playground/Contents.swift
+++ Help.playground/Contents.swift
@@ -1,6 +1,18 @@
+//let name = "Audrey"
+//name.uppercaseString
+
 let name = "Audrey"
 name.uppercaseString
 
+
+//let uppercaseName = name.uppercaseString
+//print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
+//// prints "Audrey... Audrey... AUDREY! WAKE UP!")
+
 let uppercaseName = name.uppercaseString
-print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
-// prints "Audrey... Audrey... AUDREY! WAKE UP!")
+print ("\(name)...\(name)...\(uppercaseName)! WAKE UP!")
+
+
+
+
+// this lab focuses on how methods are called like functions, except you put the constant or variable you are call on them before the method name, followed by a dot, and then the name of the method. Arguments to the method are supplied the same way they are when you call functions: By placing them in parentheses after the method name. For now, just think of a method as a function. You'll learn a lot more about methods in a later lesson.
